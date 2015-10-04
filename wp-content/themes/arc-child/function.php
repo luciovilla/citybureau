f09249d7b544e634c12fe60c4c2ea70cc2f15b1b
Lucio Villa <contact@luciovilla.com>
2015-10-04 11:30:11 -0500

    tracking child theme now

---
+++ wp-content/themes/arc-child/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 4/13/2015
+ * Time: 12:00 PM
+ */
---
+++ wp-content/themes/arc-child/template-interim.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Interim Home
+*/
+get_header(); ?>
+
+
+
+<?php	the_content(); ?>
+
+<style type="text/css">
+.mc4wp-form {
+	width: 100%;
+    max-width: 400px;
+    margin: 0 auto 10%;
+}
+#footer, .vc_custom_1427785190873 {
+	display: none;
+}
+.st_1443824089 {
+    padding: 5px;
+}
+
+</style>
+
+<?php get_footer(); ?>
