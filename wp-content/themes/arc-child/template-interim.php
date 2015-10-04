e868345aeedf31fbaa24d38117757a29c56a3c09
Lucio Villa <contact@luciovilla.com>
2015-10-04 18:39:29 -0500

    updated inerim homepage

--- wp-content/themes/arc-child/template-interim.php
+++ wp-content/themes/arc-child/template-interim.php
@@ -5,20 +5,23 @@ Template Name: Interim Home
 get_header(); ?>
 
 
+<?php while ( have_posts() )
+    the_post();
+    the_content(); ?>
 
-<?php	the_content(); ?>
 
 <style type="text/css">
 .mc4wp-form {
-	width: 100%;
-    max-width: 400px;
-    margin: 0 auto 10%;
+    width: 100%;
+    max-width: 460px;
+    margin: 0 auto;
+    padding: 0 40px;
 }
 #footer, .vc_custom_1427785190873 {
 	display: none;
 }
 .st_1443824089 {
-    padding: 5px;
+    padding: 5px !important;
 }
 
 </style>
